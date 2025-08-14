13855a1688fd8aa1543ae9ff97209a7ae7f4309f
Don Morgan <don.morgan@engagestar.com>
2025-08-14 12:11:25 -0500

    Fixing publishing dependency

--- .github/Publish.ps1
+++ .github/Publish.ps1
@@ -1,6 +1,8 @@
 #Workaround for known issue: https://github.com/PowerShell/PSResourceGet/issues/1806
 Get-PSResourceRepository | out-null
 
+#Install required module or Publish-PsResource will fail
+Install-Module BusinessCentralApi -Scope CurrentUser -Force
 
 $ModulePath = ".\Module\"
 Publish-PSResource -Path $ModulePath -ApiKey $Env:APIKEY
